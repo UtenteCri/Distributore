d022929a8ce340255865b2cac07deac760c0c8c5
Cristina Rodio <Cristina Rodio@LAPTOP-SB645259>
2023-11-13 20:27:27 +0100

    secondo

--- Distributore/src/it/corvallis/esercizi/DistributoreDiesel.java
+++ Distributore/src/it/corvallis/esercizi/DistributoreDiesel.java
@@ -5,7 +5,6 @@ public class DistributoreDiesel extends Distributore {
 	public DistributoreDiesel(String proprietario, String citta, String nome_distributore, int serbatoio,
 			float prezzo) {
 		super(proprietario, citta, nome_distributore, serbatoio, prezzo);
-		// TODO Auto-generated constructor stub
 	}
 
 }
